993480bff95c5daff3dc9a15d54e17b3b8eb96cb
jgyj123 <jonathangohyj@gmail.com>
2022-05-20 14:46:28 +0800

    added basic login functionality

--- orbital/src/App.js
+++ orbital/src/App.js
@@ -1,12 +1,19 @@
 import { React } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Login from "./Login";
 import Navbar from "./Navbar";
 import Game from "./Game";
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Game />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Game />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/game" element={<Game />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 };
