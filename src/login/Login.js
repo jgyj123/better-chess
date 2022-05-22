23ad8e9f9e36592070af1b649485c927570c65d3
Ezra <ezratio15195@gmail.com>
2022-05-23 00:15:39 +0800

    Navbar fix

--- src/login/Login.js
+++ src/login/Login.js
@@ -85,107 +85,110 @@ export const Login = () => {
   // };
   const navigate = useNavigate();
   return (
-    <Container
-      maxW="lg"
-      py={{
-        base: "12",
-        md: "24",
-      }}
-      px={{
-        base: "0",
-        sm: "8",
-      }}
-    >
-      <Stack spacing="8">
-        <Stack spacing="6">
-          <img src={logo} alt="Logo" />
-          <Stack
-            spacing={{
-              base: "2",
-              md: "3",
-            }}
-            textAlign="center"
-          >
-            <Heading
-              size={useBreakpointValue({
-                base: "s",
-                md: "sm",
-              })}
-            >
-              Log in to your account
-            </Heading>
-            <HStack spacing="1" justify="center">
-              <Text color="muted">Don&apos;t have an account?</Text>
-              <Button variant="link" colorScheme="blue">
-                Sign up
-              </Button>
-            </HStack>
-          </Stack>
-        </Stack>
-        <Box
-          py={{
-            base: "0",
-            sm: "8",
-          }}
-          px={{
-            base: "4",
-            sm: "10",
-          }}
-          bg="white"
-          boxShadow={{
-            base: "none",
-            sm: useColorModeValue("md", "md-dark"),
-          }}
-          borderRadius={{
-            base: "none",
-            sm: "xl",
-          }}
-        >
+    <div>
+      <Navbar />
+      <Container
+        maxW="lg"
+        py={{
+          base: "12",
+          md: "24",
+        }}
+        px={{
+          base: "0",
+          sm: "8",
+        }}
+      >
+        <Stack spacing="8">
           <Stack spacing="6">
-            <Stack spacing="5">
-              <FormControl>
-                <FormLabel htmlFor="email">Email</FormLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  value={loginEmail}
+            <img src={logo} alt="Logo" />
+            <Stack
+              spacing={{
+                base: "2",
+                md: "3",
+              }}
+              textAlign="center"
+            >
+              <Heading
+                size={useBreakpointValue({
+                  base: "s",
+                  md: "sm",
+                })}
+              >
+                Log in to your account
+              </Heading>
+              <HStack spacing="1" justify="center">
+                <Text color="muted">Don&apos;t have an account?</Text>
+                <Button variant="link" colorScheme="blue">
+                  Sign up
+                </Button>
+              </HStack>
+            </Stack>
+          </Stack>
+          <Box
+            py={{
+              base: "0",
+              sm: "8",
+            }}
+            px={{
+              base: "4",
+              sm: "10",
+            }}
+            bg="white"
+            boxShadow={{
+              base: "none",
+              sm: useColorModeValue("md", "md-dark"),
+            }}
+            borderRadius={{
+              base: "none",
+              sm: "xl",
+            }}
+          >
+            <Stack spacing="6">
+              <Stack spacing="5">
+                <FormControl>
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                  <Input
+                    id="email"
+                    type="email"
+                    value={loginEmail}
+                    onChange={(e) => {
+                      setLoginEmail(e.target.value);
+                    }}
+                  />
+                </FormControl>
+
+                <ButtonMod text="hi" loadingText="loading" />
+                <PasswordField
+                  value={loginPassword}
                   onChange={(e) => {
-                    setLoginEmail(e.target.value);
+                    setLoginPassword(e.target.value);
                   }}
                 />
-              </FormControl>
-
-              <ButtonMod text="hi" loadingText="loading" />
-              <PasswordField
-                value={loginPassword}
-                onChange={(e) => {
-                  setLoginPassword(e.target.value);
-                }}
-              />
-            </Stack>
-            <HStack justify="space-between">
-              <Checkbox defaultChecked>Remember me</Checkbox>
-              <Button variant="link" colorScheme="blue" size="sm">
-                Forgot password?
-              </Button>
-            </HStack>
-            <Stack spacing="6">
-              <Button colorScheme="messenger" onClick={login}>
-                Sign in
-              </Button>
-              <HStack>
-                <Divider />
-                <Text fontSize="sm" whiteSpace="nowrap" color="muted">
-                  or continue with
-                </Text>
-                <Divider />
+              </Stack>
+              <HStack justify="space-between">
+                <Checkbox defaultChecked>Remember me</Checkbox>
+                <Button variant="link" colorScheme="blue" size="sm">
+                  Forgot password?
+                </Button>
               </HStack>
-              <OAuthButtonGroup signInWithGoogle={loginWithGoogle} />
+              <Stack spacing="6">
+                <Button colorScheme="messenger" onClick={login}>
+                  Sign in
+                </Button>
+                <HStack>
+                  <Divider />
+                  <Text fontSize="sm" whiteSpace="nowrap" color="muted">
+                    or continue with
+                  </Text>
+                  <Divider />
+                </HStack>
+                <OAuthButtonGroup signInWithGoogle={loginWithGoogle} />
+              </Stack>
             </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Container>
+          </Box>
+        </Stack>
+      </Container>
+    </div>
   );
 };
 export default Login;
