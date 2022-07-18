3d3747761549cef51346f42b5bcabddbe33fd2a6
Ezra <ezratio15195@gmail.com>
2022-07-18 10:27:17 +0800

    Add new boards

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -2,10 +2,14 @@ import React, { Component, useState } from "react";
 import { Box, Container, Heading, Grid } from "@chakra-ui/react";
 import Navbar from "../components/Navbar/Navbar";
 import Card from "./ItemCard";
-import chessboard1 from "../Shop/shopItems/ChessBoard.png";
+import chessboard1 from "../Shop/shopItems/ChessBoard1.png";
 import chessboard2 from "../Shop/shopItems/ChessBoard2.png";
 import chessboard3 from "../Shop/shopItems/ChessBoard3.png";
 import chessboard4 from "../Shop/shopItems/ChessBoard4.png";
+import chessboard5 from "../Shop/shopItems/ChessBoard5.png";
+import chessboard6 from "../Shop/shopItems/ChessBoard6.png";
+import chessboard7 from "../Shop/shopItems/ChessBoard7.png";
+import chessboard8 from "../Shop/shopItems/ChessBoard8.png";
 
 export default function Shop(props) {
   return (
@@ -55,6 +59,38 @@ export default function Shop(props) {
           color1={"Black"}
           color2={"Gray"}
           itemID={3}
+        />
+        <Card
+          name={"Pink Blosson"}
+          price={"750"}
+          image={chessboard5}
+          color1={"Light Pink"}
+          color2={"Milk"}
+          itemID={4}
+        />
+        <Card
+          name={"Woody"}
+          price={"1100"}
+          image={chessboard6}
+          color1={"Coconut"}
+          color2={"Silver Brown"}
+          itemID={5}
+        />
+        <Card
+          name={"Calm Cyan"}
+          price={"2100"}
+          image={chessboard7}
+          color1={"Sky Blue"}
+          color2={"White"}
+          itemID={6}
+        />
+        <Card
+          name={"Smooth Brown"}
+          price={"3000"}
+          image={chessboard8}
+          color1={"French Beige"}
+          color2={"Cornsilk"}
+          itemID={7}
         />
       </Grid>
     </Box>
