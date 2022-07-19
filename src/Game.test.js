b804a5fb813fdf93dbb8e6bc13f302b59d24a195
Ezra <ezratio15195@gmail.com>
2022-07-19 17:47:15 +0800

    Add Game test (rendering)

--- src/Game.test.js
+++ src/Game.test.js
@@ -1,8 +1,8 @@
-import App from "./App";
 import { createRoot } from "react-dom/client";
+import Game from "./Game";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   const root = createRoot(div);
-  root.render(<App />);
+  root.render(<Game />);
 });
